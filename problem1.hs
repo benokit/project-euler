63ac35ca18d5434ca79ce6a70df09197858a9f2f
benokit <benjamin.batistic@adacta-fintech.com>
2023-07-10 13:19:04 +0200

    problem 1

--- problem1.hs
+++ problem1.hs
@@ -3,7 +3,7 @@
 -- Find the sum of all the multiples of $3$ or $5$ below $1000$.
 
 sumOfMultiples :: Int -> Int
-sumOfMultiples n = sum $ takeWhile (< 1000) $ map (* n) [1,2..]
+sumOfMultiples n = let m = div 999 n in n * (div (m * (m + 1)) 2)
 
 main :: IO ()
 main = print $ sumOfMultiples 3 + sumOfMultiples 5 - sumOfMultiples 15
