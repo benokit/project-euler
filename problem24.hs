b92760c6078b38b28010817630a4d799a69ec942
benokit <benjamin.batistic@adacta-fintech.com>
2023-07-15 19:22:41 +0200

    problem 24

--- problem24.hs
+++ problem24.hs
@@ -4,13 +4,13 @@ A permutation is an ordered arrangement of objects. For example, 3124 is one pos
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 --}
 
-putInFront :: [a] -> Int -> [a]
-putInFront xs n = let xs' = drop (n - 1) xs in head xs' : (take (n - 1) xs ++ tail xs')
-
 permutations :: [a] -> [[a]]
 permutations [x] = [[x]]
-permutations xs = foldr1 (++) $ map subPermutations $ map (putInFront xs) [1 .. (length xs)]
-    where subPermutations (y:ys) = map (\ys' -> y:ys') $ permutations ys
+permutations xs = f [] xs
+    where
+        f ys [] = []
+        f ys' (y:ys) = (g y (ys' ++ ys)) ++ (f (ys' ++ [y]) ys)
+        g y ys = map ((:) y) $ permutations ys
 
 main :: IO()
 main = print $ head $ drop 999999 $ permutations [0..9]
