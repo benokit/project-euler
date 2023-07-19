f7085c94a365bf3162629b8e771abf33426ac569
benokit <benjamin.batistic@adacta-fintech.com>
2023-07-19 12:25:29 +0200

    problem 39

---
+++ problem39.hs
@@ -0,0 +1,20 @@
+{--
+If $p$ is the perimeter of a right angle triangle with integral length sides, $\{a, b, c\}$, there are exactly three solutions for $p = 120$.
+$\{20,48,52\}$, $\{24,45,51\}$, $\{30,40,50\}$
+For which value of $p \le 1000$, is the number of solutions maximised?
+--}
+
+import Data.List (maximumBy)
+import Data.Ord (comparing)
+
+f :: Int -> Int -> Int -> Int
+f o a b = o * o - 2 * (o * (a + b) - a * b)
+
+test :: Int -> (Int, Int) -> Bool
+test p (a, b) = let c = p - a - b in a * a + b * b == c * c
+
+triangleCount :: Int -> Int
+triangleCount p = length $ filter (test p) [(a, b) | a <- [1..(p - 1)], b <- [a..(p - 1)], a + b < p && p - (a + b) > a && p - (a + b) > b]
+
+main :: IO()
+main = print $ fst $ maximumBy (comparing snd) $ map (\n -> (n, triangleCount n)) [1..1000]
